6bd29c2b84f80d506f3cc55e535dba70d07012a7
CaioVicenzi <80120724+CaioVicenzi@users.noreply.github.com>
2022-06-27 12:07:50 -0300

    Melhorei a experiência do usuário
    Concertei um problema que tava dando no meu computador na hora de compilar, ele não estava aceitando a declaração do i dentro do for
    Depois disso eu ainda adicionei mensagem de circuito concluido com sucesso
    e um system("PAUSE") depois de exibir os pilotos no console

--- formula1.2.c
+++ formula1.2.c
@@ -9,17 +9,17 @@ char paises[20][100] = {
 	"Alemanha",
 	"Brasil",	
 	"Argentina",	
-	"FranÁa",
-	"¡ustria",
-	"Austr·lia",
-	"Finl‚ndia",
-	"It·lia",
+	"Fran√ßa",
+	"√Åustria",
+	"Austr√°lia",
+	"Finl√¢ndia",
+	"It√°lia",
 	"Espanha",	
 	"Estados Unidos",
-	"Canad·",
-	"¡frica do Sul",
-	"Nova Zel‚ndia",
-	"PaÌses Baixos"
+	"Canad√°",
+	"√Åfrica do Sul",
+	"Nova Zel√¢ndia",
+	"Pa√≠ses Baixos"
 };
 
 struct Piloto{
@@ -50,7 +50,7 @@ struct MelhorVolta{
 };
 
 struct Circuito{
-	// ˙nico gerado automaticamente com base no ˙ltimo circuito registrado
+	// √∫nico gerado automaticamente com base no √∫ltimo circuito registrado
 	int codigo;
 	char nome[100];
 	char pais[100];
@@ -77,26 +77,30 @@ int main() {
 	int pilotosJaRegistrados = 0;
 	int opcaoUsuario;
 	
-	//	Para o cadastro de um Piloto ou Circuito sÛ poder„o ser utilizados os paÌses
-	//	previamente cadastrados (seja um vetor prÈ-definido ou com o uso de leitura em
+	//	Para o cadastro de um Piloto ou Circuito s√≥ poder√£o ser utilizados os pa√≠ses
+	//	previamente cadastrados (seja um vetor pr√©-definido ou com o uso de leitura em
 	//	arquivos).
 	
 	while(1){
 		
+		//exibe o menu para o usu√°rio e pede que ele escolha uma das op√ß√µes abaixo:
+		system("CLS");
 		printf("BEM VINDO AO SISTEMA DA FORMULA 1\n\n");
 		printf("1) Cadastrar um novo piloto\n");
 		printf("2) Cadastrar um novo circuito\n");
 		printf("3) Exibir pilotos cadastrados\n");
 		printf("4) Sair do programa\n");
-		printf("Digite a sua opÁ„o: ");
+		printf("Digite a sua op√ß√£o: ");
 		scanf("%i", &opcaoUsuario);
 		
+		// baseado no que o usu√°rio digitou, um dos caminhos ser√° escolhido.
 		switch(opcaoUsuario){
 			case 1: 
 				pilotosJaRegistrados = incluiNovoPiloto(pilotosRegistrados, pilotosJaRegistrados);
 				break;
 			case 2:
 				circuitosJaRegistrados=incluiNovoCircuito(circuitosRegistrados,circuitosJaRegistrados);
+				break;
 			case 3:
 				exibePilotos(pilotosRegistrados, pilotosJaRegistrados);
 				break;
@@ -109,27 +113,30 @@ int main() {
 
 // aqui eu vou fazer um programa que exibe todos os pilotos registrados:
 void exibePilotos(struct Piloto pilotos[399], int quantidadePilotos){
-	
-	// para cada um dos pilotos que est„o dentro do vetor passado como argumento para a funÁ„o
-	// ele imprime v·rias informaÁıes sobre o piloto em quest„o.
+	int i;
+	// para cada um dos pilotos que est√£o dentro do vetor passado como argumento para a fun√ß√£o
+	// ele imprime v√°rias informa√ß√µes sobre o piloto em quest√£o.
 	system("CLS");
 	printf("============= PILOTOS REGISTRADOS ============\n\n");
 
-	for(int i = 0; i < quantidadePilotos; i++){
-		// imprimindo o cÛdigo do piloto, nome, idade, paÌs de origem e sexo.
-		printf("Piloto n˙mero: %i\n", pilotos[i].codigo);
-		printf("Nome: %s\n", pilotos[i].nome);
-		printf("Idade: %d\n", pilotos[i].idade);
-		printf("PaÌs de origem: %s\n", pilotos[i].paisOrigem);
+	for(i = 0; i < quantidadePilotos; i++){
+		struct Piloto piloto = pilotos[i];
+		// imprimindo o c√≥digo do piloto, nome, idade, pa√≠s de origem e sexo.
+		printf("Piloto n√∫mero: %i\n", piloto.codigo);
+		printf("Nome: %s\n", piloto.nome);
+		printf("Idade: %d\n", piloto.idade);
+		printf("Pa√≠s de origem: %s\n", piloto.paisOrigem);
 		printf("Sexo: ");
-		// verifica se o sexo salvo È f, ent„o o piloto È do sexo feminino, caso contr·rio, o sexo do
-		// piloto em quest„o È masculino.
-		if(pilotos[i].sexo == 'f') 
+		// verifica se o sexo salvo √© f, ent√£o o piloto √© do sexo feminino, caso contr√°rio, o sexo do
+		// piloto em quest√£o √© masculino.
+		if(piloto.sexo == 'f') 
 			printf("feminino\n");
 		else printf("masculino\n");
 	}
 	
-	printf("\n============================================\n");
+	printf("\n============================================\n\n");
+	
+	system("PAUSE");
 }
 
 int incluiNovoCircuito(struct Circuito circuitosRegistrados[500], int circuitosJaRegistrados){
@@ -138,27 +145,33 @@ int incluiNovoCircuito(struct Circuito circuitosRegistrados[500], int circuitosJ
 	circuito.codigo = circuitosJaRegistrados;
 	pedeNome(circuito.nome, "circuito");
 	pedePais(circuito.pais, "circuito");
+	
+	// entrada do tamanho do circuito a ser adicionado
 	printf("Digite o tamanho do circuito: ");
 	scanf("%f", &circuito.tamanho);
 	fflush(stdin);
-	// falta incluir as melhores voltas do percurso, que ser· incluÌda em outra funcionalidade, por enquanto
-	// o circuito ficar· sem uma melhor volta atÈ que o usu·rio insira uma em um circuito especifico.
-		
+	// falta incluir as melhores voltas do percurso, que ser√° inclu√≠da em outra funcionalidade, por enquanto
+	// o circuito ficar√° sem uma melhor volta at√© que o usu√°rio insira uma em um circuito especifico.
+	
 	circuitosRegistrados[circuitosJaRegistrados] = circuito;
+	
+	//exibe mensagem de sucesso, deixa esperar 2 segundos e depois retorna o valor passado da quantidade de circuitos j√° registrados no sistema somando 1
+	printf("\n\nCircuito adicionado com sucesso!");
+	Sleep(2000);
 	return circuitosJaRegistrados + 1;
 }
 
 int incluiNovoPiloto(struct Piloto pilotosRegistrados[399], int pilotosJaRegistrados){
 	struct Piloto piloto;
 	
-	// preenche as informaÁıes do piloto que est· sendo cadastrado
+	// preenche as informa√ß√µes do piloto que est√° sendo cadastrado
 	piloto.codigo = pedeIdPiloto(pilotosRegistrados, pilotosJaRegistrados);
 	pedeNome(piloto.nome, "piloto");
 	piloto.idade = pedeIdade();
 	pedePais(piloto.paisOrigem, "piloto");
 	piloto.sexo = pedeSexo();
 	
-	// adiciona o piloto criado nessa funÁ„o dentro do vetor de pilotos.
+	// adiciona o piloto criado nessa fun√ß√£o dentro do vetor de pilotos.
 	pilotosRegistrados[pilotosJaRegistrados] = piloto;
 	printf("\nPiloto Registrado com sucesso!");
 	
@@ -194,7 +207,7 @@ int incluirMelhorVolta(){
 	melhorVolta.data.dia = atoi(diaString);
 	melhorVolta.data.mes = atoi(mesString);
 	melhorVolta.data.ano = atoi(anoString);
-
+	
 	return 0;
 }
 
@@ -214,7 +227,7 @@ int pedeIdade(){
 	
 	if(idade > 0 && idade < 200) return idade;
 	system("CLS");
-	printf("Idade inv·lida! Tente novamente...\n");
+	printf("Idade inv√°lida! Tente novamente...\n");
 	return pedeIdade();
 }
 
@@ -224,14 +237,14 @@ void pedePais(char paisOrigem[100], char pedido[100]){
 	char pais[100];
 	strcpy(pais, paisOrigem);
 	
-	printf("Digite o paÌs de origem do %s: ", pedido);
+	printf("Digite o pa√≠s de origem do %s: ", pedido);
 	scanf("%[^\n]s", &pais);
 	fflush(stdin);
-	// essa vari·vel indica se o programa encontrou o nome do paÌs que o usu·rio digitou no vetor de paÌses
-	// 0 significa que n„o encontrou, 1 significa que encontrou o nome
+	// essa vari√°vel indica se o programa encontrou o nome do pa√≠s que o usu√°rio digitou no vetor de pa√≠ses
+	// 0 significa que n√£o encontrou, 1 significa que encontrou o nome
 	int achou = 0;
 
-	//validaÁ„o do pais origem:
+	//valida√ß√£o do pais origem:
 
 	for(i = 0; i < 20; i++){
 		if(strcmp(pais, paises[i]) == 0)
@@ -240,7 +253,7 @@ void pedePais(char paisOrigem[100], char pedido[100]){
 	
 	if(achou == 0){
 		system("CLS");
-		printf("N„o conseguimos entender... tente novamente\n");
+		printf("N√£o conseguimos entender... tente novamente\n");
 		pedePais(paisOrigem, pedido);	
 	}
 }
@@ -248,27 +261,27 @@ void pedePais(char paisOrigem[100], char pedido[100]){
 
 int pedeIdPiloto(struct Piloto pilotosRegistrados[399], int pilotosJaRegistrados){
 	
-	// falta verificar se o id que o usu·rio passou j· foi incluÌdo em algum momento, assim evitar a repetiÁ„o do mesmo piloto
-	int idPiloto;
+	// falta verificar se o id que o usu√°rio passou j√° foi inclu√≠do em algum momento, assim evitar a repeti√ß√£o do mesmo piloto
+	int idPiloto, i;
 	
-	// pedindo a identificaÁ„o do piloto
+	// pedindo a identifica√ß√£o do piloto
 	printf("Numero de identificacao do piloto:");
 	scanf("%i",&idPiloto);
 	fflush(stdin);
 	
-	// verifica se o id do piloto est· no intervalo de 100 e 500 (n„o incluso) 
+	// verifica se o id do piloto est√° no intervalo de 100 e 500 (n√£o incluso) 
 	
-	for(int i = 0; i < pilotosJaRegistrados; i++){
+	for(i = 0; i < pilotosJaRegistrados; i++){
 		if(idPiloto == pilotosRegistrados[i].codigo){
 			system("CLS");
-			printf("ERRO! ID J¡ REGISTRADO, TENTE NOVAMENTE...\n");
+			printf("ERRO! ID J√Å REGISTRADO, TENTE NOVAMENTE...\n");
 			return pedeIdPiloto(pilotosRegistrados, pilotosJaRegistrados);
 		}
 	}
 	if(idPiloto > 100 && idPiloto < 500){
 		return idPiloto;
 	} else { 
-	// se o id do piloto n„o passou pelas verificaÁıes acima, ent„o o programa limpa a tela, exibe uma mensagem de erro e comeÁa o processo novamente atÈ achar um id v·lido
+	// se o id do piloto n√£o passou pelas verifica√ß√µes acima, ent√£o o programa limpa a tela, exibe uma mensagem de erro e come√ßa o processo novamente at√© achar um id v√°lido
 		system("CLS");
 		printf("Valor digitado incorreto, tente novamente...\n\n");
 		return pedeIdPiloto(pilotosRegistrados, pilotosJaRegistrados);
@@ -278,15 +291,15 @@ int pedeIdPiloto(struct Piloto pilotosRegistrados[399], int pilotosJaRegistrados
 char pedeSexo(){
 	char sexo;
 	
-	// pedindo para o usu·rio um char que represente o sexo do piloto
+	// pedindo para o usu√°rio um char que represente o sexo do piloto
 	printf("Digite o sexo do piloto (m -> masculino, f -> feminino): ");
 	scanf("%c", &sexo);
 	fflush(stdin);
 	
-	// se for f ou m, ent„o est· v·lido, portanto j· retorna o valor
+	// se for f ou m, ent√£o est√° v√°lido, portanto j√° retorna o valor
 	if(sexo == 'f' || sexo == 'm') return sexo;
 	
-	// caso o contr·rio exibe a mensagem de valor inv·lido e repete a funÁ„o
-	printf("Valor inv·lido! Tente novamente...\n");
+	// caso o contr√°rio exibe a mensagem de valor inv√°lido e repete a fun√ß√£o
+	printf("Valor inv√°lido! Tente novamente...\n");
 	return pedeSexo();	
 }
